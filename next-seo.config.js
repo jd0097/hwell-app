b5a10dd180e3e8fc77ffe53681a26589148e0171
jd0097 <dh.react123@gmail.com>
2023-09-20 10:36:35 +0900

    네이버

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,18 +1,23 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-    titleTemplate: '%s - 서비스 페이지',
-    openGraph: {
-      type: 'website',
-      site_name: '건강증진센터 위치보기 서비스',
-      images: [
-        { url: 'https://nextjs.org/static/blog/next-13/twitter-card.png' },
-      ],
-    },
-    additionalLinkTags: [
-      {
-        rel: 'shortcut icon',
-        href: '/favicon.ico',
-      },
+  titleTemplate: "%s - 서비스 페이지",
+  openGraph: {
+    type: "website",
+    site_name: "건강증진센터 위치보기 서비스",
+    images: [
+      { url: "https://nextjs.org/static/blog/next-13/twitter-card.png" },
     ],
-  };
-
+  },
+  additionalLinkTags: [
+    {
+      rel: "shortcut icon",
+      href: "/favicon.ico",
+    },
+  ], 
+  additionalMetaTags: [
+    {
+      name: "naver-site-verification",
+      content: "617a4d67d231a2ea8cb3caa65170d8644b4c8f4d",
+    },
+  ],
+};
